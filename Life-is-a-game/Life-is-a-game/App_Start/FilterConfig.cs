cfc38328438d8c51330bfc7882034252ac7c7604
Dedulko Nikita <36064675+DedulkoN@users.noreply.github.com>
2020-09-03 13:55:29 +0300

    Create project

---
+++ Life-is-a-game/Life-is-a-game/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Life_is_a_game
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Life-is-a-game/Life-is-a-game/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Life_is_a_game.MvcApplication" Language="C#" %>
